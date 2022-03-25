f51161b21b18aad3ce05ad50492976c48910ea72
Cihsan <mutamuty@gmial.com>
2022-03-25 22:17:41 +0600

    display book name in UI

--- src/component/BookShop/BookShop.js
+++ src/component/BookShop/BookShop.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BookShop = () => {
+    const [books,setBook]=useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setBook(data))
+    },[])
     return (
         <div>
-                <h1>Book Shop</h1>
+            {
+                books.map(book=><h5>{book.name}</h5>)
+            }
         </div>
     );
 };
