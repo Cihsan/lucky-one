1dfb378a09c7f8709695905427a39ab877729a92
Cihsan <mutamuty@gmial.com>
2022-03-25 22:10:59 +0600

    added two component

---
+++ src/component/BookShop/BookShop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookShop = () => {
+    return (
+        <div>
+                <h1>Book Shop</h1>
+        </div>
+    );
+};
+
+export default BookShop;
---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome To Book Store</h1>
+        </div>
+    );
+};
+
+export default Header;
